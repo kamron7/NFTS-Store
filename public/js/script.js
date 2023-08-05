df345f4040d31dc205aa0788d97f2b489e274329
Kamron <kamronrustambekov22@gmail.com>
2023-08-05 15:55:42 +0500

    ver1.0

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+const ull = document.querySelector('.slider');
+const btn = document.querySelector('.icon');
+const btn2 = document.querySelector('.icon2');
+
+btn.addEventListener('click', function () {
+    ull.classList.toggle('active');
+})
+
+btn2.addEventListener('click', function () {
+    ull.classList.toggle('active');
+})
